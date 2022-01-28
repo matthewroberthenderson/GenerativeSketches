eb7ac019ef264c2ac54c6381bcf35cf9d572c549
Matthew Henderson <32535159+matthewroberthenderson@users.noreply.github.com>
2022-01-28 19:15:58 +1100

    Pushing Main
    Pushing Main

---
+++ SequenceA/main.cpp
@@ -0,0 +1,9 @@
+#include "ofMain.h"
+#include "ofApp.h"
+
+//========================================================================
+int main( ){
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+
+}
